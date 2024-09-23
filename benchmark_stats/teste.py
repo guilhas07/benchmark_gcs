736bfeebaf2ce63b3e7204563409b0c2bf683072
Guilherme Soares <guilhermesoares1970@gmail.com>
2024-09-24 00:09:11 +0100

    feat: add all files

---
+++ benchmark_stats/teste.py
@@ -0,0 +1,28 @@
+import glob
+from collections import defaultdict
+import json
+
+
+graal = "GraalVM"
+oracle = "HotSpot"
+d = defaultdict(set)
+for f in glob.glob("*.json"):
+    if graal in f:
+        d[graal].add(f.replace(graal, ""))
+    elif oracle in f:
+        d[oracle].add(f.replace(oracle, ""))
+
+
+print(f"{len(d[graal])} == {len(d[oracle])}")
+# print(f"{json.dumps(d[graal])} {len(d[graal])}")
+# print(d[oracle])
+
+assert len(d[graal]) == len(d[oracle])
+print(type(d[graal]))
+print(type(d[oracle]))
+# assert d[graal] == d[oracle]
+
+
+for i in d[graal]:
+    if i not in d[oracle]:
+        print(i)
